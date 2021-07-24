c3df28da36f6efe3d299fa26f7ce33d94f492c87
Peggy-Pei <peggyguo@Peggyde-MacBook-Air.local>
2021-07-24 17:31:28 +0800

    add function

---
+++ products2.py
@@ -0,0 +1,61 @@
+import os
+
+#讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品, 價格' in line:
+                continue
+            name, price= line.strip().split(',')
+            products.append([name, price])
+        print(products)
+
+    
+    return products
+
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格:')
+        p = []
+        p.append(name)
+        p.append(price)
+        products.append(p)
+    # or products.append([name, price])
+
+    print(products)
+    return products
+
+
+def print_product(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
+
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('商品, 價格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('YA! 找到檔案了！')
+    else:
+        print('找不到檔案')
+    products = read_file('products.csv')
+    products = user_input(products)
+    print_product(products)
+    write_file('products.csv', products)
+
+main()
+
+
+
+
+
+
+
